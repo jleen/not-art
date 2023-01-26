173570029f775bdcd9dad7ea4a6ccfdd7d8edc0f
John Leen <jleen@saturnvalley.org>
2023-01-26 12:00:59 -0500

    Art cannot be art

---
+++ build.py
@@ -0,0 +1,9 @@
+import dryck
+import shutil
+
+class GptContext(dryck.HtmlContext):
+    def sep(self):
+        return '<p class="sep">✼</p>'
+
+dryck.render(GptContext(), 'gpt.dryck', 'index.html.dryck', 'dist/index.html')
+shutil.copy('style.css', 'dist/style.css')
