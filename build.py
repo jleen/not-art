ac5c97ee1fac7a9a9eeacbedbe4ad2bf1e771478
John Leen <jleen@saturnvalley.org>
2023-01-26 23:25:31 -0500

    Nitpick list formatting

--- build.py
+++ build.py
@@ -7,5 +7,8 @@ class GptContext(dryck.HtmlContext):
     def sep(self):
         return '<p class="sep">✼</p>'
 
+    def li(self, body):
+        return f'<li><span>{body}</span></li>\n'
+
 dryck.render(GptContext(), 'gpt.dryck', 'index.html.dryck', 'dist/index.html')
 shutil.copy('style.css', 'dist/style.css')
